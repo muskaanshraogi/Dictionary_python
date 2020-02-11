7918e4b82b126668e2ccf52b0e40cfed9b07ed59
Muskaan Shraogi <54302920+muskaanshraogi@users.noreply.github.com>
2020-02-11 19:41:57 +0530

    Suggestions for wrong words

--- Dictionary/mydictionary.py
+++ Dictionary/mydictionary.py
@@ -1,27 +1,40 @@
 
 import json
-import sys
+import difflib
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
 def search(word):
-    return data[word]
-
-while True:
-	word = str(input("\nEnter a word to be searched\n(Type 'byebye' to end the program) :"))
-	
-	if(word == 'byebye'):
-		print("\nBye Bye!\n")
-		break
 	try:
-		meaning = search(word)
-		
+		meaning = data[word]
+			
 		print("\nThe meaning of the word '" + word + "' is :\n")
 		for i in range(len(meaning)):
 			print(str(i+1) + ". " + str(meaning[i]))
 			
 	except:
-		print("\nRequested word not found!\n")
+		match = get_close_matches(word, data.keys())
+		if len(match) != 0:
+			ch = input("\nDid you mean '" + str(match[0]) + "' ? y/n : ")
+			if ch == 'y' or ch == 'Y':
+				search(match[0])
+			else:
+				print("\nRequested word not found!")
+		else:
+			print("\nRequested word not found!")
+
+
+while True:
+	word = str(input("\nEnter a word to be searched\n(Type 'byebye' to end the program) :"))
+	word = word.lower()
+	
+	if(word == 'byebye'):
+		print("\nBye Bye!\n")
+		break
+	
+	search(word)
+	
 	
 	
 	
