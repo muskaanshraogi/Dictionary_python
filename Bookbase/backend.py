3ef1e8c28e8ffdc6ac88ea73891d239a8ac79c2b
Muskaan Shraogi <54302920+muskaanshraogi@users.noreply.github.com>
2020-03-03 19:53:50 +0530

    Validations

--- Bookbase/backend.py
+++ Bookbase/backend.py
@@ -1,51 +1,36 @@
 import sqlite3
 
-def create():
-    con = sqlite3.connect("books.db")
-    cur = con.cursor()
-    con.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title TEXT, author TEXT, year INTEGER, isbn INTEGER)")
-    con.commit()
-    con.close()
+class database:
 
-def insert(title,author,year,isbn):
-    con = sqlite3.connect("books.db")
-    cur = con.cursor()
-    con.execute("INSERT INTO book VALUES (NULL,?,?,?,?)",(title,author,year,isbn))
-    con.commit()
-    con.close()
+    def __init__(self):
+        self.con = sqlite3.connect("books.db")
+        self.cur = self.con.cursor()
+        self.con.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title TEXT, author TEXT, year INTEGER, isbn INTEGER)")
+        self.con.commit()
+        
 
-def view():
-    con = sqlite3.connect("books.db")
-    cur = con.cursor()
-    cur.execute("SELECT * FROM book")
-    rows = cur.fetchall()
-    con.close()
-    return rows
+    def insert(self,title,author,year,isbn):
+        self.con.execute("INSERT INTO book VALUES (NULL,?,?,?,?)",(title,author,year,isbn))
+        self.con.commit()
+    
 
-def search(title="", author="", year="", isbn=""):
-    con = sqlite3.connect("books.db")
-    cur = con.cursor()
-    cur.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR isbn=?",(title,author,year,isbn))
-    rows = cur.fetchall()
-    con.close()
-    return rows
+    def view(self):
+        self.cur.execute("SELECT * FROM book")
+        rows = self.cur.fetchall()
+        return rows
 
-def delete(id):
-    con = sqlite3.connect("books.db")
-    cur = con.cursor()
-    con.execute("DELETE FROM book WHERE id=?",(id,))
-    con.commit()
-    con.close()
+    def search(self,title="", author="", year="", isbn=""):
+        self.cur.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR isbn=?",(title,author,year,isbn))
+        rows = self.cur.fetchall()
+        return rows
 
-def update(id,title,author,year,isbn):
-    con = sqlite3.connect("books.db")
-    cur = con.cursor()
-    con.execute("UPDATE book SET title=?, author=?, year=?, isbn=? WHERE id=?",(title,author,year,isbn,id))
-    con.commit()
-    con.close()
+    def delete(self,id):
+        self.con.execute("DELETE FROM book WHERE id=?",(id,))
+        self.con.commit()
 
-create()
-#insert("Harry Potter","JK Rowling",1997,197877611)
-#insert("The Fault in our Stars","John Green",2002,19755551)
-#insert("The Alchemist","Paulo Coelho",1988,192389002)
-#print(view())
+    def update(self,id,title,author,year,isbn):
+        self.con.execute("UPDATE book SET title=?, author=?, year=?, isbn=? WHERE id=?",(title,author,year,isbn,id))
+        self.con.commit()
+
+    def __del__(self):
+        self.con.close()
--- Bookbase/script.py
+++ Bookbase/script.py
@@ -1,45 +1,55 @@
 from tkinter import *
-import backend
+from backend import database
 
+db = database()
 
-def get_selection(event):
-    global selected_tuple
-    index = box.curselection()[0]
-    selected_tuple = box.get(index)
-    e1.delete(0,END)
-    e1.insert(END,selected_tuple[1])
-    e2.delete(0,END)
-    e2.insert(END,selected_tuple[2])
-    e3.delete(0,END)
-    e3.insert(END,selected_tuple[3])
-    e4.delete(0,END)
-    e4.insert(END,selected_tuple[4])
-    
+class interface:
 
-def viewall_command():
-    box.delete(0,END)
-    for row in backend.view():
-        box.insert(END,row)
+    def get_selection(self,event):
+        global selected_tuple
+        index = box.curselection()[0]
+        selected_tuple = box.get(index)
+        e1.delete(0,END)
+        e1.insert(END,selected_tuple[1])
+        e2.delete(0,END)
+        e2.insert(END,selected_tuple[2])
+        e3.delete(0,END)
+        e3.insert(END,selected_tuple[3])
+        e4.delete(0,END)
+        e4.insert(END,selected_tuple[4])
+        
 
-def search_record():
-    box.delete(0,END)
-    for row in backend.search(title_text.get(),author_text.get(),year_text.get(),isbn_text.get()):
-        box.insert(END,row)
+    def viewall_command(self):
+        box.delete(0,END)
+        for row in db.view():
+            box.insert(END,row)
 
-def add_record():
-    box.delete(0,END)
-    if backend.search(isbn=isbn_text.get()) == []:
-        backend.insert(title_text.get(),author_text.get(),year_text.get(),isbn_text.get())
+    def search_record(self):
+        box.delete(0,END)
+        for row in db.search(title_text.get(),author_text.get(),year_text.get(),isbn_text.get()):
+            box.insert(END,row)
+
+    def add_record(self):
+        box.delete(0,END)
+        if db.search(isbn=isbn_text.get()) == []:
+            if(title_text.get() != ""):
+                db.insert(title_text.get(),author_text.get(),year_text.get(),isbn_text.get())
+                box.insert(END,(title_text.get(),author_text.get(),year_text.get(),isbn_text.get()))
+            else:
+                box.insert(END,"Enter a book name to add.")
+        else:
+            box.insert(END,"The isbn already exists.")
+
+    def update_record(self):
+        db.update(selected_tuple[0],title_text.get(),author_text.get(),year_text.get(),isbn_text.get())
+        box.delete(0,END)
         box.insert(END,(title_text.get(),author_text.get(),year_text.get(),isbn_text.get()))
 
-def update_record():
-    backend.update(selected_tuple[0],title_text.get(),author_text.get(),year_text.get(),isbn_text.get())
-    box.delete(0,END)
-    box.insert(END,(title_text.get(),author_text.get(),year_text.get(),isbn_text.get()))
+    def delete_record(self):
+        db.delete(selected_tuple[0])
+        self.viewall_command()
 
-def delete_record():
-    backend.delete(selected_tuple[0])
-    viewall_command()
+interface=interface()
 
 
 window = Tk()
@@ -74,7 +84,7 @@ e4.grid(row=1,column=3)
 
 box = Listbox(window,height=6,width=35)
 box.grid(row=2,column=0,rowspan=6,columnspan=2)
-box.bind('<<ListboxSelect>>',get_selection)
+box.bind('<<ListboxSelect>>',interface.get_selection)
 
 scroll1 = Scrollbar(window)
 scroll1.grid(row=2,column=2,rowspan=6)
@@ -88,22 +98,22 @@ scroll2.grid(row=7,column=0,columnspan=2)
 box.configure(xscrollcommand=scroll2.set)
 scroll2.configure(command=box.xview)
 
-b1 = Button(window,text="Search",width=13,command=search_record)
+b1 = Button(window,text="Search",width=13,command=interface.search_record)
 b1.grid(row=2,column=3)
 
-b2 = Button(window,text="Add",width=13,command=add_record)
+b2 = Button(window,text="Add",width=13,command=interface.add_record)
 b2.grid(row=3,column=3)
 
-b3 = Button(window,text="View All",width=13,command=viewall_command)
+b3 = Button(window,text="View All",width=13,command=interface.viewall_command)
 b3.grid(row=4,column=3)
 
-b4 = Button(window,text="Update",width=13,command=update_record)
+b4 = Button(window,text="Update",width=13,command=interface.update_record)
 b4.grid(row=5,column=3)
 
-b5 = Button(window,text="Delete",width=13,command=delete_record)
+b5 = Button(window,text="Delete",width=13,command=interface.delete_record)
 b5.grid(row=6,column=3)
 
-b6 = Button(window,text="Close",width=13)
+b6 = Button(window,text="Close",width=13,command=window.destroy)
 b6.grid(row=7,column=3)
 
 window.mainloop()
